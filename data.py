7baa1216178930d71fb3c340c254de88c437241e
Sharada1415 <ramyar.alpha@gmail.com>
2025-08-15 12:46:35 +0530

    Defined the class Sample

--- data.py
+++ data.py
@@ -1,15 +1,16 @@
 str_ = "selenium"
 
-def finding_length(iterable):
-    length = 0
-    for i in iterable:
-        length += 1
 
-    print(length)
+class Sample:
 
-finding_length(str_)
-finding_length([1, 2, 3, 4, 5, 6, 7, 8, 9])
-finding_length('Ashwini')
+    def finding_length(self, iterable):
+        length = 0
+        for i in iterable:
+            length += 1
+
+        print(length)
+
+
 
 
 
